5be92a011a305ba4ae3e438f292b01008d014d63
Semiranov <semiranov@gmail.com>
2023-08-16 23:55:59 +0300

    HW1

---
+++ src/HW1/ModelElements/Flash.java
@@ -0,0 +1,17 @@
+package HW1.ModelElements;
+
+public class Flash {
+    Point3D location;
+    Angle3D angle;
+    Color color;
+    float power;
+
+
+    public void Rotate(Angle3D grad) {
+
+    }
+
+    public void Move(Point3D cm) {
+
+    }
+}
